f0151b5def483a8b3f43f61eb98b76d58ff740af
Enock EGO <115041750+EGOproject@users.noreply.github.com>
2023-02-08 00:41:07 +0300

    Update main.py

--- slot machine/main.py
+++ slot machine/main.py
@@ -9,8 +9,41 @@ COLS = 3
 ROWS = 3
 
 symbol_count = {
-    
+    "A":2,
+    "B":4,
+    "C":6,
+    "D":8
 }
+
+def get_slot_machine_spin(rows, cols, symbols):
+    all_symbols = []
+    for symbol, symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+
+    columns = []
+    for col in range(cols):
+        column = []
+        current_symbols = all_symbols[:]
+        for row in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove()
+            column.append(value)
+        
+        columns.append(column)
+    return columns
+
+def print_slot_machine(columns):
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i != len(columns) -1:
+                print(column[row], "|")
+            else:
+                print(column[row], "|")
+
+
+
+
 def deposit ():
     while True:
         amount = input("DEPOSIT: $")

bb01933d8ee0c59a18456db077edf20f6828a893
Enock EGO <115041750+EGOproject@users.noreply.github.com>
2023-03-17 14:27:10 +0300

    EGotube, a youtube video downloader

---
+++ EGOtube.py
@@ -0,0 +1,33 @@
+# this is a simple program that can be used to download youtube videos
+#credit to tuomaskijiova and patrickloeber
+
+from pytube import YouTube
+from tqdm import tqdm, trange
+import time
+
+print("Welcome to EGOtube")
+print()
+link = input("Paste the link: ")
+
+eyt = YouTube(link)
+print()
+print("Video Title: ", eyt.title)
+print("Video Views: ", eyt.views)
+
+print()
+print("Downloading...")
+
+eyd = eyt.streams.get
+
+eyd.download("/users/public/downloads")
+
+print("Compiling Resources...")
+
+with tqdm(total=100) as pbar:
+    for i in range(10):
+        time.sleep(0.1)
+        pbar.update(1)
+print("Download Completed: ", eyd.download)
+
+
+

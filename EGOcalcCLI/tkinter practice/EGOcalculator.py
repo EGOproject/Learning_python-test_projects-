7f3b2fb612da44c2b22fc4bc9fe9d39b2d28a962
Enock EGO <115041750+EGOproject@users.noreply.github.com>
2023-03-18 06:53:35 +0300

    Tkinter GUI calc

--- EGOcalcCLI/tkinter practice/EGOcalculator.py
+++ EGOcalcCLI/tkinter practice/EGOcalculator.py
@@ -1,3 +1,7 @@
+#this is a GUI calculater by EGOproject.
+#credits:CODEMY.COM, EGOproject
+
+
 from tkinter import *
 
 root = Tk()
@@ -9,7 +13,63 @@ calc_screen.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
 #when a button is clicked
 
 def button_click(number):
-    calc_screen.insert(0, number)
+    current = calc_screen.get()
+    calc_screen.delete(0, END)
+    calc_screen.insert(0, str(current) + str(number))
+
+def clear_screen():
+    calc_screen.delete(0, END)
+    
+def button_plus():
+    first_number = calc_screen.get()
+    global num1
+    global operation
+    operation = "plus"
+    num1 = int(first_number)
+    calc_screen.delete(0, END)
+
+def button_minus():
+    first_number = calc_screen.get()
+    global num1
+    global operation
+    operation = "minus"
+    num1 = int(first_number)
+    calc_screen.delete(0, END)
+
+def button_times():
+    first_number = calc_screen.get()
+    global num1
+    global operation
+    operation = "times"
+    num1 = int(first_number)
+    calc_screen.delete(0, END)
+
+def button_share():
+    first_number = calc_screen.get()
+    global num1
+    global operation
+    operation = "share"
+    num1 = int(first_number)
+    calc_screen.delete(0, END)
+
+def button_ans():
+    second_number = calc_screen.get()
+    calc_screen.delete(0, END)
+    if operation == "plus":
+        sum = num1 + int(second_number)
+        calc_screen.insert(0 , sum)
+    elif operation == "minus":
+        diff = num1 - int(second_number)
+        calc_screen.insert(0 , diff)
+    elif operation == "times":
+        prod = num1 * int(second_number)
+        calc_screen.insert(0 , prod)
+    elif operation == "share":
+        quo = num1 / int(second_number)
+        calc_screen.insert(0 , int(quo))
+    
+
+
 #buttons
 #number buttons
 button_1 = Button(root, text="1", bg="black", fg="white", padx=30, pady=15, command=lambda: button_click(1))
@@ -21,19 +81,19 @@ button_6 = Button(root, text="6", bg="black", fg="white", padx=30, pady=15, comm
 button_7 = Button(root, text="7", bg="black", fg="white", padx=30, pady=15, command=lambda: button_click(7))
 button_8 = Button(root, text="8", bg="black", fg="white", padx=30, pady=15, command=lambda: button_click(8))
 button_9 = Button(root, text="9", bg="black", fg="white", padx=30, pady=15, command=lambda: button_click(9))
-button_0 = Button(root, text="0", bg="black", fg="silver", padx=70, pady=15, command=lambda: button_click(0))
+button_0 = Button(root, text="0", bg="black", fg="white", padx=105, pady=15, command=lambda: button_click(0))
 
 #function Buttons
 button_dec = Button(root, text="•", bg="gray", fg="red", padx=30, pady=15, command=lambda: button_click())
-button_equ = Button(root, text="=", bg="green", fg="black", padx=30, pady=45, command=lambda: button_click())
-button_plus = Button(root, text="➕", bg="gray", fg="blue", padx=25, pady=45, command=lambda: button_click())
-button_minus = Button(root, text="➖", bg="gray", fg="blue", padx=25, pady=15, command=lambda: button_click())
-button_times = Button(root, text="✖", bg="gray", fg="blue", padx=25, pady=15, command=lambda: button_click())
-button_div = Button(root, text="➗", bg="gray", fg="blue", padx=25, pady=15, command=lambda: button_click())
-button_cls = Button(root, text="CLEAR", bg="red", fg="black", padx=15, pady=15, command=lambda: button_click())
+button_equ = Button(root, text="=", bg="green", fg="black", padx=30, pady=45, command=button_ans)
+button_add = Button(root, text="➕", bg="gray", fg="blue", padx=25, pady=45, command=button_plus)
+button_sub = Button(root, text="➖", bg="gray", fg="blue", padx=25, pady=15, command=button_minus)
+button_mul = Button(root, text="✖", bg="gray", fg="blue", padx=25, pady=15, command=button_times)
+button_div = Button(root, text="➗", bg="gray", fg="blue", padx=25, pady=15, command=button_share)
+button_cls = Button(root, text="CLEAR", bg="red", fg="black", padx=15, pady=15, command=clear_screen)
 
 #posting them to GUI
-button_0.grid(row=5, columnspan=2, column=0)
+button_0.grid(row=5, columnspan=3, column=0)
 
 button_1.grid(row=4, column=0)
 button_2.grid(row=4, column=1)
@@ -49,10 +109,10 @@ button_9.grid(row=2, column=2)
 
 button_cls.grid(row=1, column=0)
 button_div.grid(row=1, column=1)
-button_times.grid(row=1, column=2)
-button_minus.grid(row=1, column=3)
-button_plus.grid(rowspan=2, row=2, column=3)
+button_mul.grid(row=1, column=2)
+button_sub.grid(row=1, column=3)
+button_add.grid(rowspan=2, row=2, column=3)
 button_equ.grid(rowspan=2, row=4, column=3)
-button_dec.grid(row=5, column=2)
+# button_dec.grid(row=5, column=2)
 
 root.mainloop()

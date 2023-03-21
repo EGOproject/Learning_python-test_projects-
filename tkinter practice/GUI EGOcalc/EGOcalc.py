997acedbb53c7167bff3f9a2a1cd64a2de6e141a
Enock EGO <115041750+EGOproject@users.noreply.github.com>
2023-03-21 22:20:53 +0300

    GUI EGOcalc update

--- tkinter practice/GUI EGOcalc/EGOcalc.py
+++ tkinter practice/GUI EGOcalc/EGOcalc.py
@@ -8,6 +8,7 @@ import math
 
 root = Tk()
 root.title("GUI EGOcalc")
+root.iconbitmap("tkinter practice/GUI EGOcalc/favicon/favicon.ico")
 
 myfont = font.Font(size = 20)
 
@@ -116,33 +117,44 @@ def button_ans():
 
 #buttons
 #number buttons
-button_1 = Button(root, text="1", bg="black", fg="white", padx=30, pady=15, command=lambda: button_click(1))
-button_2 = Button(root, text="2", bg="black", fg="white", padx=30, pady=15, command=lambda: button_click(2))
-button_3 = Button(root, text="3", bg="black", fg="white", padx=30, pady=15, command=lambda: button_click(3))
-button_4 = Button(root, text="4", bg="black", fg="white", padx=30, pady=15, command=lambda: button_click(4))
-button_5 = Button(root, text="5", bg="black", fg="white", padx=30, pady=15, command=lambda: button_click(5))
-button_6 = Button(root, text="6", bg="black", fg="white", padx=30, pady=15, command=lambda: button_click(6))
-button_7 = Button(root, text="7", bg="black", fg="white", padx=30, pady=15, command=lambda: button_click(7))
-button_8 = Button(root, text="8", bg="black", fg="white", padx=30, pady=15, command=lambda: button_click(8))
-button_9 = Button(root, text="9", bg="black", fg="white", padx=30, pady=15, command=lambda: button_click(9))
-button_0 = Button(root, text="0", bg="black", fg="white", padx=105, pady=15, command=lambda: button_click(0))
+button_1 = Button(root, text="1", bg="orangered", fg="white", padx=17.5, pady=5, command=lambda: button_click(1))
+button_2 = Button(root, text="2", bg="orangered", fg="white", padx=17.5, pady=5, command=lambda: button_click(2))
+button_3 = Button(root, text="3", bg="orangered", fg="white", padx=17.5, pady=5, command=lambda: button_click(3))
+button_4 = Button(root, text="4", bg="orangered", fg="white", padx=17.5, pady=5, command=lambda: button_click(4))
+button_5 = Button(root, text="5", bg="orangered", fg="white", padx=17.5, pady=5, command=lambda: button_click(5))
+button_6 = Button(root, text="6", bg="orangered", fg="white", padx=17.5, pady=5, command=lambda: button_click(6))
+button_7 = Button(root, text="7", bg="orangered", fg="white", padx=17.5, pady=5, command=lambda: button_click(7))
+button_8 = Button(root, text="8", bg="orangered", fg="white", padx=17.5, pady=5, command=lambda: button_click(8))
+button_9 = Button(root, text="9", bg="orangered", fg="white", padx=17.5, pady=5, command=lambda: button_click(9))
+button_0 = Button(root, text="0", bg="orangered", fg="white", padx=95, pady=-10, command=lambda: button_click(0))
 
 #function Buttons
-button_sq = Button(root, text="∧2", bg="black", fg="yellow", padx=5, pady=5, command=button_square,)
-button_sqr = Button(root, text="∨2", bg="black", fg="yellow", padx=5, pady=5, command=button_sroot)
-button_cb = Button(root, text="∧3", bg="black", fg="yellow", padx=5, pady=5, command=button_cube)
-button_cbr = Button(root, text="∨3", bg="black", fg="yellow", padx=5, pady=5, command=button_croot)
-button_equ = Button(root, text="=", bg="green", fg="black", padx=30, pady=45, command=button_ans)
-button_add = Button(root, text="➕", bg="gray", fg="blue", padx=25, pady=45, command=button_plus)
-button_sub = Button(root, text="➖", bg="gray", fg="blue", padx=25, pady=15, command=button_minus)
-button_mul = Button(root, text="✖", bg="gray", fg="blue", padx=25, pady=15, command=button_times)
-button_div = Button(root, text="➗", bg="gray", fg="blue", padx=25, pady=15, command=button_share)
-button_cls = Button(root, text="CLEAR", bg="red", fg="black", padx=15, pady=15, command=clear_screen)
+button_sq = Button(root, text="∧2", bg="orange", fg="white", padx=5, pady=5, command=button_square,)
+button_sqr = Button(root, text="∨2", bg="orange", fg="white", padx=5, pady=5, command=button_sroot)
+button_cb = Button(root, text="∧3", bg="orange", fg="white", padx=5, pady=5, command=button_cube)
+button_cbr = Button(root, text="∨3", bg="orange", fg="white", padx=5, pady=5, command=button_croot)
+button_equ = Button(root, text="=", bg="darkgreen", fg="white", padx=18, pady=30, command=button_ans)
+button_add = Button(root, text="➕", bg="green", fg="white", padx=24, pady=45, command=button_plus)
+button_sub = Button(root, text="➖", bg="green", fg="white", padx=24, pady=15, command=button_minus)
+button_mul = Button(root, text="✖", bg="green", fg="white", padx=24, pady=15, command=button_times)
+button_div = Button(root, text="➗", bg="green", fg="white", padx=24, pady=15, command=button_share)
+button_cls = Button(root, text="CLEAR", bg="maroon", fg="white", padx=15, pady=15, command=clear_screen)
 
 button_sq["font"] = myfont
 button_sqr["font"] = myfont
 button_cb["font"] = myfont
 button_cbr["font"] = myfont
+button_equ["font"] = myfont
+button_0["font"] = myfont
+button_1["font"] = myfont
+button_2["font"] = myfont
+button_3["font"] = myfont
+button_4["font"] = myfont
+button_5["font"] = myfont
+button_6["font"] = myfont
+button_7["font"] = myfont
+button_8["font"] = myfont
+button_9["font"] = myfont
 
 #posting them to GUI
 button_0.grid(row=6, columnspan=3, column=0)
@@ -171,5 +183,8 @@ button_sqr.grid(row=1, column=1)
 button_cb.grid(row=1, column=2)
 button_cbr.grid(row=1, column=3)
 # button_dec.grid(row=5, column=2)
+
+button_quit = Button(root, text="Exit program", command=root.quit, bg="maroon", fg="white", padx=115, pady=1,)
+button_quit.grid(row=7, columnspan=4)
 
 root.mainloop()
--- tkinter practice/tkinter_lectures_summary.py
+++ tkinter practice/tkinter_lectures_summary.py
@@ -1,18 +1,25 @@
 from tkinter import *
+import tkinter.font as font
 
 root = Tk()
 root.title("Simple Greeter")
+root.iconbitmap("tkinter practice/GUI EGOcalc/favicon/favicon.ico")
+
+my_font = font.Font(size=20)
+button_quit = Button(root, text="Exit program", command=root.quit)
+button_quit.pack()
 
 name = Entry(root, width=50, bg="teal", fg="white", borderwidth=5)
 name.pack()
 name.insert(0, "Enter name to Greet!!")
 name.get()
  
-def onclick():
-    myLabel = Label(root, text="Hello " + name.get() + " 😁")
+def onclick(number):
+    myLabel = Label(root, text="Hello " + name.get() + f" 😁 {number}")
     myLabel.pack()
 
-my_button = Button(root, text="Greet", command=onclick, padx=10, pady = 1, fg="white", bg="black")
+my_button = Button(root, text="Greet", command=lambda: onclick(0), padx=10, pady = 1, fg="white", bg="black")
+my_button["font"] = my_font
 my_button.pack()
 
 root.mainloop()
@@ -37,6 +44,12 @@ pady = padding y-axis
 fg = button text color
 bg = button color
 command=lambda: button_click(1) = to pass a value from a button
+for button text size
+import tkinter.font as font
+---create an object, name it(myFont) and do the following
+    myFont = font.Font(size=15)
+    my_button["font"] = myFont
+
 """
 
 #input --- entry widget
@@ -45,4 +58,18 @@ can modify its appearance
 name.get() function grabs the input entered
 name.insert() function creates a place holder for the input
 
+"""
+
+#images these are manipulated in the following ways.
+"""
+for icons
+---root.iconbitmap("location of the image)
+"""
+
+#buttons 2
+
+"""
+for the exit button
+--button_quit = Button(root, command=root.quit)
+--button_quit.pack()
 """

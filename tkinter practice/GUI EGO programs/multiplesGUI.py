92dc0b6ed4036533bdc819080ddb1fe8d2987fdb
Enock EGO <115041750+EGOproject@users.noreply.github.com>
2023-03-24 21:13:07 +0300

    changes to GUI multiples

--- tkinter practice/GUI EGO programs/multiplesGUI.py
+++ tkinter practice/GUI EGO programs/multiplesGUI.py
@@ -4,5 +4,5 @@ from tkinter.font import Font
 root = Tk()
 root.title("Derive Multiples GUI")
 root.iconbitmap("tkinter practice/GUI EGO programs/favicon/favicon.ico")
-root.geometry("400x400")
+root.geometry("400x600")
 root.mainloop()

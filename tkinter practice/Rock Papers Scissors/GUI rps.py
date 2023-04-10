31e1c04a2105611cb2422307ff14daf2c5c431d6
Enock EGO <115041750+EGOproject@users.noreply.github.com>
2023-04-10 13:37:37 +0300

    RPS GUI update

--- tkinter practice/Rock Papers Scissors/GUI rps.py
+++ tkinter practice/Rock Papers Scissors/GUI rps.py
@@ -18,24 +18,123 @@ number_font = font.Font(size = 30)
 cbg = "#1b428b"
 ubg = "#508705"
 
-uw = 0
-ul = 0
+engine = pyttsx3.init()
+engine.setProperty("rate", 150)
 
+global uw
+uw = 0
+global ul
+ul = 0
+global cw
 cw = 0
+global cl
 cl = 0
 
-draws = 0
-
-#logic of the game
-
-uv =""
-cv = ""
-
-
-def win_draw_lose(player):
+def selection(uv):
+    cv = random.choice([1,2,3])
     if uv == cv:
-        pass
+        s_string = "DRAW"
+        u_screen.delete(0, END)
+        u_screen.insert(0, s_string)
+        c_screen.delete(0, END)
+        c_screen.insert(0, s_string)
+        pyttsx3.speak("DRAW")
+        # time.sleep(1)
+    elif uv ==1:
+        # pyttsx3.speak("ROCK")
+        if uv == 1:
+            # if cv == 1:
+            #     cs_string = "ROCK"
+            #     c_screen.delete(0, END)
+            #     c_screen.insert(0, cs_string)
+                # pyttsx3.speak("ROCK")
+            #     # time.sleep(1)
+            if cv == 2:
+                cw +=1
+                ul +=1
+                cs_string = f"      {cw}            {cl}"
+                us_string = f"      {uw}            {ul}"
+                c_screen.delete(0, END)
+                c_screen.insert(0, cs_string)
+                u_screen.delete(0, END)
+                u_screen.insert(0, us_string)
+                pyttsx3.speak("PAPER")
+                # time.sleep(1)
+            elif cv == 3:
+                uw +=1
+                cl +=1
+                cs_string = f"      {cw}            {cl}"
+                us_string = f"      {uw}            {ul}"
+                u_screen.delete(0, END)
+                u_screen.insert(0, us_string)
+                c_screen.delete(0, END)
+                c_screen.insert(0, cs_string)
+                pyttsx3.speak("SCISSORS")
+    elif uv ==2:
+        # pyttsx3.speak("PAPER")
+        if uv == 2:
+            if cv == 1:
+                uw +=1
+                cl +=1
+                cs_string = f"      {cw}            {cl}"
+                us_string = f"      {uw}            {ul}"
+                u_screen.delete(0, END)
+                u_screen.insert(0, us_string)
+                c_screen.delete(0, END)
+                c_screen.insert(0, cs_string)
+                pyttsx3.speak("ROCK")
+                # time.sleep(1)
+            # elif cv == 2:
+            #     cs_string = "PAPER"
+            #     c_screen.delete(0, END)
+            #     c_screen.insert(0, cs_string)
+            #     pyttsx3.speak("PAPER")
+                # time.sleep(1)
+            elif cv == 3:
+                cw +=1
+                ul +=1
+                cs_string = f"      {cw}            {cl}"
+                us_string = f"      {uw}            {ul}"
+                c_screen.delete(0, END)
+                c_screen.insert(0, cs_string)
+                u_screen.delete(0, END)
+                u_screen.insert(0, us_string)
+                pyttsx3.speak("SCISSORS")
+                # time.sleep(1)
+    elif uv == 3:
+        # pyttsx3.speak("SCISSORS")
+        if uv == 3:
+            if cv == 1:
+                cw +=1
+                ul +=1
+                cs_string = f"      {cw}            {cl}"
+                us_string = f"      {uw}            {ul}"
+                c_screen.delete(0, END)
+                c_screen.insert(0, cs_string)
+                u_screen.delete(0, END)
+                u_screen.insert(0, us_string)
+                pyttsx3.speak("ROCK")
+                # time.sleep(1)
+            elif cv == 2:
+                uw +=1
+                cl +=1
+                cs_string = f"      {cw}            {cl}"
+                us_string = f"      {uw}            {ul}"
+                u_screen.delete(0, END)
+                u_screen.insert(0, us_string)
+                c_screen.delete(0, END)
+                c_screen.insert(0, cs_string)
+                pyttsx3.speak("PAPER")
+                # time.sleep(1)
+            # elif cv == 3:
+            #     cs_string = "SCISSORS"
+            #     c_screen.delete(0, END)
+            #     c_screen.insert(0, cs_string)
+            #     pyttsx3.speak("SCISSORS")
+                # time.sleep(1)
+    
 
+    
 
 #   appearance of the gui
 head_label = Label(root, text="ROCK PAPER SCISSORS", bg="#1e6c6c")
@@ -46,29 +145,16 @@ U_label = Label(root, text="USER", bg=ubg)
 U_label.grid(row=1, column=0, columnspan = 3, ipadx=110)
 U_label["font"] = SIDE_font
 
-rock_button = Button(root, text="💎", bg=ubg)
+rock_button = Button(root, text="💎", bg=ubg, command=lambda: selection(1))
 rock_button.grid(row=2, column=0, padx=10,pady= 20)
-paper_button = Button(root, text="📃", bg=ubg)
+paper_button = Button(root, text="📃", bg=ubg, command=lambda: selection(2))
 paper_button.grid(row=2, column=1, padx=10,pady= 20)
-scissor_button = Button(root, text="✂", bg=ubg)
+scissor_button = Button(root, text="✂", bg=ubg, command=lambda: selection(3))
 scissor_button.grid(row=2, column=2, padx=10,pady= 20)
 
 rock_label = Label(root, text="ROCK", bg=ubg).grid(row=3, column=0, ipadx=25)
 paper_label = Label(root, text="PAPER", bg=ubg).grid(row=3, column=1, ipadx=25)
 scissor_label = Label(root, text="SCISSORS", bg=ubg).grid(row=3, column=2, ipadx=25)
-
-u_win_label = Label(root, text="WINS        LOSSES", bg=ubg)
-u_win_label.grid(row=4, column=0, columnspan=3, ipadx=45,pady=5)
-u_win_label["font"] = SIDE_font
-
-utstring = f"{uw}           {ul}"
-uw_label = Label(root, text=utstring, fg="green")
-uw_label.grid(row=5, column=0, columnspan=3, ipadx=30)
-uw_label["font"] = number_font
-
-# ul_label = Label(root, text=str(ul), fg="red")
-# ul_label.grid(row=5, column=1, columnspan=2, ipadx=40)
-# ul_label["font"] = number_font
 
 #button fonts
 rock_button["font"] = HEAD_font
@@ -80,34 +166,36 @@ C_label.grid(row=1, column=3, columnspan = 3, ipadx=80)
 C_label["font"] = SIDE_font
 
 
-crock_button = Button(root, text="💎", bg=cbg)
+crock_button = Button(root, text="💎", bg=cbg, state=DISABLED)
 crock_button.grid(row=2, column=3, padx=10,pady= 20)
-cpaper_button = Button(root, text="📃", bg=cbg)
+cpaper_button = Button(root, text="📃", bg=cbg, state=DISABLED)
 cpaper_button.grid(row=2, column=4, padx=10,pady= 20)
-cscissor_button = Button(root, text="✂", bg=cbg)
+cscissor_button = Button(root, text="✂", bg=cbg, state=DISABLED)
 cscissor_button.grid(row=2, column=5, padx=10,pady= 20)
 
 rock_label = Label(root, text="ROCK", bg=cbg).grid(row=3, column=3, ipadx=25)
 paper_label = Label(root, text="PAPER", bg=cbg).grid(row=3, column=4, ipadx=25)
 scissor_label = Label(root, text="SCISSORS", bg=cbg).grid(row=3, column=5, ipadx=25)
 
-c_win_label = Label(root, text="WINS        LOSSES", bg=cbg)
-c_win_label.grid(row=4, column=3, columnspan=3, ipadx=45, pady=5)
-c_win_label["font"] = SIDE_font
-
-ctstring = f"{cw}           {cl}"
-uw_label = Label(root, text=ctstring, fg="blue")
-uw_label.grid(row=5, column=3, columnspan=3, ipadx=30)
-uw_label["font"] = number_font
-
 #button fonts
 crock_button["font"] = HEAD_font
 cpaper_button["font"] = HEAD_font
 cscissor_button["font"] = HEAD_font
 
-dtstring = f"{draws}    DRAWS"
-draws_label = Label(root, text=dtstring)
-draws_label.grid(row=6, column=0, columnspan=6, ipadx=5, pady=10)
-draws_label["font"] = number_font
+u_win_label = Label(root, text="WINS        LOSSES", bg=ubg)
+u_win_label.grid(row=4, column=0, columnspan=3, ipadx=45,pady=5)
+u_win_label["font"] = SIDE_font
+
+u_screen = Entry(root, width=15)
+u_screen.grid(row=5, column=0, columnspan=3)
+u_screen["font"] =HEAD_font
+
+c_win_label = Label(root, text="WINS        LOSSES", bg=cbg)
+c_win_label.grid(row=4, column=3, columnspan=3, ipadx=45,pady=5)
+c_win_label["font"] = SIDE_font
+
+c_screen = Entry(root, width=15)
+c_screen.grid(row=5, column=3, columnspan=3)
+c_screen["font"] =HEAD_font
 
 root.mainloop()

21eb17a2b879aa389b5426a11d7061018974157a
Enock EGO <115041750+EGOproject@users.noreply.github.com>
2023-06-03 12:13:54 +0300

    update

---
+++ bootcamp/lectures.py
@@ -0,0 +1,19 @@
+# indexing
+"""
+var = "hello"
+
+print(varr[1:3]) #this will print values in index 1 and 2 and ignore 3
+NB: negative indexing
+"""
+
+#lists
+# update list
+"""
+list = ["a", "b", "c", "d", "e"]
+print(list)
+list[2] ="z"
+print(list)
+"""
+#tuple()
+# cannot be changed but can be deleted as a whole.
+
